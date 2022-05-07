f47ad3e8c052f6bb5612cd083bcc96c032a9e4ab
daberry_wh <daberry@williamhill.us>
2022-05-07 08:32:07 -0700

    creating backend

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,90 @@
+const launchesDatabase = require("./launches.mongo");
+const planets = require("./planets.mongo");
+
+let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Expoloration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  target: "Kepler-442 b",
+  customers: ["NASA", "ZTM"],
+  upcoming: true,
+  success: true,
+};
+
+saveLaunch(launch);
+
+async function existsLaunchWithId(launchId) {
+  return launchesDatabase.findOne({ flightNumber: launchId });
+}
+
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort("-flightNumber");
+
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
+}
+
+async function getAllLaunches() {
+  return await launchesDatabase.find({}, { _id: 0, __v: 0 });
+}
+
+async function saveLaunch(launch) {
+  const planet = await planets.findOne({ keplerName: launch.target });
+  if (!planet) {
+    throw new Error("No matching planet was found");
+  }
+
+  await launchesDatabase.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
+}
+
+async function scheduleNewLaunch(launch) {
+  const newFlightNumber = (await getLatestFlightNumber()) + 1;
+  const newLaunch = Object.assign(launch, {
+    success: true,
+    upcoming: true,
+    customers: ["ZTM", "NASA"],
+    flightNumber: newFlightNumber,
+  });
+
+  await saveLaunch(newLaunch);
+}
+
+async function abortLaunchById(launchId) {
+  // const aborted = launches.get(launchId);
+  // aborted.upcoming = false;
+  // aborted.success = false;
+
+  const aborted = await launchesDatabase.updateOne(
+    {
+      flightNumber: launchId,
+    },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+  console.log(aborted);
+
+  return aborted.acknowledged && aborted.modifiedCount === 1;
+}
+
+module.exports = {
+  getAllLaunches,
+  existsLaunchWithId,
+  abortLaunchById,
+  scheduleNewLaunch,
+};
---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const mongoose = require("mongoose");
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URL =
+  "mongodb+srv://daylanberry:v33srFPCOUbI7ywr@nasacluster.oyprd.mongodb.net/nasa?retryWrites=true&w=majority";
+
+const server = http.createServer(app);
+
+mongoose.connection.once("open", () => {
+  console.log("mongo connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+async function startServer() {
+  await mongoose.connect(MONGO_URL);
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
+}
+
+startServer();
