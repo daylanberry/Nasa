5c46dd832cfa37cbeb627da2854cc66022c2ad00
daberry_wh <daberry@williamhill.us>
2022-05-28 10:42:14 -0700

    backend

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
-const launchesRouter = require("./routes/launches/launches.router");
-const planetsRouter = require("./routes/planets/planets.router");
+const apiRouter = require("./routes/api");
 
 const app = express();
 app.use(
@@ -16,8 +15,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
+app.use("/v1", apiRouter);
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
--- server/src/server.js
+++ server/src/server.js
@@ -1,24 +1,17 @@
 const http = require("http");
-const mongoose = require("mongoose");
 const app = require("./app");
+const { mongoConnect } = require("./services/mongo");
 const { loadPlanetsData } = require("./models/planets.model");
+const { loadLaunchData } = require("./models/launches.model");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-const MONGO_URL = `mongodb+srv://daylanberry:${process.env.MONGO_PASS}@nasacluster.oyprd.mongodb.net/nasa?retryWrites=true&w=majority`;
 const server = http.createServer(app);
 
-mongoose.connection.once("open", () => {
-  console.log("mongo connection ready");
-});
-
-mongoose.connection.on("error", (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
